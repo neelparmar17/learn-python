ea2d8b54d2939394ef747b465696b8a3a7b32deb
Neel Parmar <neel.parmar@mountblue.io>
2018-03-14 18:42:52 +0530

    get data with only one db hit

--- automateDailyReport/Poc.py
+++ automateDailyReport/Poc.py
@@ -1,56 +1,11 @@
-import pycurl
-from io import BytesIO
-try:
-    from urllib.parse import urlencode
-except:
-    from urllib import urlencode
-
-headers = {}
-def header_function(header_line):
-    # HTTP standard specifies that headers are encoded in iso-8859-1.
-    # On Python 2, decoding step can be skipped.
-    # On Python 3, decoding step is required.
-    header_line = header_line.decode('iso-8859-1')
-
-    # Header lines include the first status line (HTTP/1.x ...).
-    # We are going to ignore all lines that don't have a colon in them.
-    # This will botch headers that are split on multiple lines...
-    if ':' not in header_line:
-        return
-
-    # Break the header line into header name and value.
-    name, value = header_line.split(':', 1)
-
-    # Remove whitespace that may be present.
-    # Header lines include the trailing newline, and there may be whitespace
-    # around the colon.
-    name = name.strip()
-    value = value.strip()
-
-    # Header names are case insensitive.
-    # Lowercase name here.
-    name = name.lower()
-
-    # Now we can actually record the header name and value.
-    # Note: this only works when headers are not duplicated, see below.
-    headers[name] = value
+loc_id = Hub.objects.filter(supplier_type__iexact="HUB", status="Active").values('name', 'supplier_ptr_id')
+    # print(hub[0])
+    for lid in loc_id:
+        vids = SupplierLocation.objects.filter(supplier_id=lid['supplier_ptr_id']).values('village_id')
+        for vid in vids:
+            com_name = Location.objects.filter(id=vid['village_id']).values('complete_name')
+            print(lid['name'] +" ==== " +com_name[0]["complete_name"])
 
 
-buffer = BytesIO()
 
-c = pycurl.Curl()
-
-c.setopt(c.URL, "http://trringology.com/users/login")
-
-post_data = {
-                'field': "1919191919",
-                'password': "marketing",
-            }
-
-post_fields = urlencode(post_data)
-c.setopt(c.POSTFIELDS, post_fields)
-c.setopt(c.HEADERFUNCTION, header_function)
-c.perform()
-print(headers.get("set-cookie"))
-print(c.getinfo(c.RESPONSE_CODE))
-c.close()
+sup_id = SupplierLocation.objects.select_related('supplier_id').filter(supplier__supplier_type__iexact="HUB", supplier__hub__status="Active").select_related('village_id').values('village_id','supplier_id', 'supplier__hub__name', 'village__complete_name')
